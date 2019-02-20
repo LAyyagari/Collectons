6cd126eda702833ba4024c6063e6b6e87cf4cba4
LAyyagari <LAyyagari1434@muleriders.saumag.edu>
2019-02-20 00:30:22 -0600

    Initial commit

---
+++ src/Hey.java
@@ -0,0 +1,29 @@
+import java.util.*;
+public class Hey
+{
+
+    public static void main(String[] args)
+    {
+        HashMap<String, String> hash_map = new HashMap<String, String>();
+
+        hash_map.put("10", "Sai");
+        hash_map.put("15", "Siri");
+        hash_map.put("20", "Sai Nidhi");
+        hash_map.put("25", "Well");
+        hash_map.put("30", "You");
+
+        System.out.println("Total People: " + hash_map);
+
+        for (Map.Entry<String,String> entry : hash_map.entrySet())
+        {
+            if (entry.getValue().startsWith("S"))
+            {
+                System.out.println(entry.getValue());
+            }
+
+        }
+
+
+
+    }
+}
